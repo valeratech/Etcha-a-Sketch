bb1516f53f472c2e89d91e4a30055f859aec36a2
Administrator <valeraryan@gmail.com>
2022-05-16 12:52:28 -0700

    Added grid button and seperated createGrid and (added) removeGrid. New grid works but functionality of adding background-color not working on new grid

--- main.js
+++ main.js
@@ -1,23 +1,45 @@
-function createGrid() {
-    // prompt('Grid Size');
-    let newGrid = document.querySelectorAll('div');
-    console.log(newGrid)
+function removeGrid() {
+    gridSize = prompt('Grid Size');
+    let newGrid = document.querySelectorAll('.container');
     for (n of newGrid) {
         n.remove();
     }
+    createGrid(gridSize);
 }
 
+function createGrid(size) {
+    footer = document.querySelector('footer')
+
+    // elements must be created inside the for loop or only one instance of the element will be created
+    // the number of containers is declared by a variable specified by the grid-button/user and executed by a for loop
+    for (let x = 0;x < size; x++) {
+        let section = document.querySelector('section');
+        let container = document.createElement('div');
+        section.insertBefore(container, footer);
+        container.className = 'container'
+
+        // loops to create a series of squares inside the container specified above
+        // this should give the same amount of squares for the x and y axis (ex. 12x12)
+        for (let y = 0; y < size; y++) {
+            let square = document.createElement('div');
+            container.append(square);
+            square.className = 'square'
+        }
+    }
+}
+
+grid = document.querySelector('.grid');
+grid.addEventListener('click', removeGrid);
 
 let square = document.querySelectorAll(".square");
-document.addEventListener("mouseover", () => {
 
+// nested even listener where the outer event listeners
+document.addEventListener("mouseover", () => {
+    // use a for loop to iterate through every square (parameter "box") and add an event listener for each one
     square.forEach(box => {
         box.addEventListener('mouseover', (e) => {
             e.target.style.backgroundColor = 'black';
         });
     });
 });
-
-grid = document.querySelector('.grid');
-grid.addEventListener('click', createGrid);
 
